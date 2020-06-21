e402df62bac0804338ad515b4915e5f980b1db86
Klyukay Evangelina <klyukay.evangelina@gmail.com>
2020-06-21 19:01:01 +0300

    Category

--- AppVelox/Controller/NewsListTableViewController.swift
+++ AppVelox/Controller/NewsListTableViewController.swift
@@ -10,32 +10,39 @@ import UIKit
 
 class NewsListTableViewController: UITableViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
-    private var previousItems: [News] = []
-    private var rssItems: [News]?
+    private let allCategoriesText = "Все категории"
+    
+    private var allNews: [News] = []
+    private var currentNews: [News]?
     private let loadParser = NetworkManager()
-    private let categories  = ["Политика", "Экономика", "Происшествия", "Общество", "Культура", "В мире", "Спорт"]
-    private var selectedCategory = "Политика"
+    private var categories: [String] = []
+    private var selectedCategoryIndex: Int = 0
+    private var selectingCategoryIndex: Int = 0
     var pickerView = UIPickerView()
     
     @IBAction func categoryPicker(_ sender: Any) {
-          let alert = UIAlertController(title: "Category", message: "\n\n\n\n\n\n", preferredStyle: .alert)
-          alert.isModalInPresentation = true
-          let pickerFrame = UIPickerView(frame: CGRect(x: 5, y: 20, width: 250, height: 140))
-          alert.view.addSubview(pickerFrame)
-          pickerFrame.dataSource = self
-          pickerFrame.delegate = self
-          alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-          alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
-              self.rssItems = self.previousItems.filter({$0.category == self.selectedCategory})
-                         self.selectedCategory = self.categories[0]
-                         self.tableView.reloadData()
-          }))
-          self.present(alert,animated: true, completion: nil)
-      }
+        if (categories.count <= 1) { return }
+        
+        let alert = UIAlertController(title: "Category", message: "\n\n\n\n\n\n", preferredStyle: .alert)
+        alert.isModalInPresentation = true
+        let pickerFrame = UIPickerView(frame: CGRect(x: 5, y: 20, width: 250, height: 140))
+        alert.view.addSubview(pickerFrame)
+        pickerFrame.dataSource = self
+        pickerFrame.delegate = self
+        pickerFrame.selectRow(selectedCategoryIndex, inComponent: 0, animated: true)
+        selectingCategoryIndex = selectedCategoryIndex
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+            self.selectedCategoryIndex = self.selectingCategoryIndex
+            self.selectNewsByCurrentCategory()
+            self.tableView.reloadData()
+        }))
+        self.present(alert,animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         refreshControl = UIRefreshControl()
         refreshControl!.addTarget(self, action: #selector(self.refresh(_:)), for: UIControl.Event.valueChanged)
         
@@ -48,30 +55,47 @@ class NewsListTableViewController: UITableViewController, UIPickerViewDataSource
     
     private func fetchData() {
         loadParser.parseFeed(url: "http://www.vesti.ru/vesti.rss") { (rssItems) in
-            self.rssItems = rssItems
-            
             DispatchQueue.main.async {
+                self.allNews = rssItems
+                self.updateCategories()
+                self.selectNewsByCurrentCategory()
                 self.tableView.reloadSections(IndexSet(integer: 0), with: .middle)
                 self.refreshControl?.endRefreshing()
             } 
         }
     }
     
-       func numberOfComponents(in pickerView: UIPickerView) -> Int {
-           return 1
-       }
-
-       func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-           return categories.count
-       }
-
-       func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-           return categories[row]
-       }
-
-       func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-           selectedCategory = categories[row]
-       }
+    private func updateCategories() {
+        categories.removeAll()
+        categories.append(allCategoriesText)
+        let availableCategories = allNews.map({ $0.category })
+        categories.append(contentsOf: Set(availableCategories))
+    }
+    
+    private func selectNewsByCurrentCategory() {
+        if selectedCategoryIndex != 0 {
+            let category = categories[selectedCategoryIndex]
+            currentNews = allNews.filter({$0.category == category})
+        } else {
+            currentNews = allNews
+        }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categories[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectingCategoryIndex = row
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
@@ -84,7 +108,7 @@ class NewsListTableViewController: UITableViewController, UIPickerViewDataSource
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let item = rssItems?[indexPath.item]
+        let item = currentNews?[indexPath.item]
         self.performSegue(withIdentifier: "showNews", sender: item)
     }
     
@@ -93,7 +117,7 @@ class NewsListTableViewController: UITableViewController, UIPickerViewDataSource
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let rssItems = rssItems else {
+        guard let rssItems = currentNews else {
             return 0
         }
         return rssItems.count
@@ -102,7 +126,7 @@ class NewsListTableViewController: UITableViewController, UIPickerViewDataSource
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsListTableViewCell
         
-        if let item = rssItems?[indexPath.item] {
+        if let item = currentNews?[indexPath.item] {
             cell.item = item
         }
         return cell
